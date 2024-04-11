5d631fd386c75a0a3bde36b096c51593615e13a7
KambhampatiHemanth <kambhampatihemanth@gmail.com>
2024-04-11 08:41:15 +0530

    third

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Header.css'
+import './Header.css';
+
 const Header = () => {
     return (
-        <div className="header">
-            <Link to="/"><img className="header__icon" src="image.png" alt="logo" /></Link>
-            <nav role="navigation" class="primary-navigation">
-            <ul>
-                <li><a href="#"><div className="wrapper">
-                    <a href="#" className="nav">
-                        <div className="one"></div>
-                        <div className="two"></div>
-                        <div className="three"></div>
-                    </a>
-                </div></a>
-                <ul class="dropdown"  style={{zIndex : "2"}} >
-                    <li><a href="https://www.linkedin.com/in/kambhampati-hemanth/" target="_blank" rel="noopener noreferrer">Profile</a></li>
-                    <li><a href="#">Contact Me</a></li>
-                    <li><a href="#">Logout</a></li>
-                </ul>
-                </li>
-            </ul>
-            </nav>
+        <div>
+            <div className="header">
+                <Link to="/"><img className="header__icon" src="image.png" alt="logo" /></Link>
+                <h1>Expense Tracker</h1> {/* Insert the heading here */}
+                <nav role="navigation" className="primary-navigation">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <div className="wrapper">
+                                    <a href="#" className="nav">
+                                        <div className="one"></div>
+                                        <div className="two"></div>
+                                        <div className="three"></div>
+                                    </a>
+                                </div>
+                            </a>
+                            <ul className="dropdown" style={{ zIndex: "2" }}>
+                                <li><a href="https://www.linkedin.com/in/kambhampati-hemanth/" target="_blank" rel="noopener noreferrer">Profile</a></li>
+                                <li><a href="#">Contact Me</a></li>
+                                <li><a href="#">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
             </div>
+        </div>
     );
 };
 
