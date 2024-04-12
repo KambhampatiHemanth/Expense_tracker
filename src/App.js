865e436867495aea059bd2ae3d4114ba2d7e8fe5
KambhampatiHemanth <kambhampatihemanth@gmail.com>
2024-04-12 18:10:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
-/*import Container from "./components/Container";
-import IncomeContainer from "./components/IncomeContainer";
-import ExpenseContainer from "./components/ExpenseContainer";
-import Form from '../src/components/Form'
-import VariablesContext from "./CustomHooks/VariablesContext";
-import Transactions from "./components/Transactions";
-function App() {
-  return (
-    <VariablesContext>
-      <div className="container">
-        <div className="child" >
-          <h2 style={{textAlign : "center"}}>Expense Tracker</h2>
-          <Container />
-          <div className="inc-exp-container">
-              <IncomeContainer />
-              <ExpenseContainer />
-          </div>
-          <h3>Add New Transaction</h3>
-          <Form />
-        </div>
-        <div className="child">
-          <h3>History</h3>
-          <Transactions />
-        </div> 
-      </div>
-    </VariablesContext>
-  );
-}
-
-export default App;*/
-
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./Header"; // Import Header component
-import Container from "./components/Container";
-import IncomeContainer from "./components/IncomeContainer";
-import ExpenseContainer from "./components/ExpenseContainer";
-import Form from '../src/components/Form'
-import VariablesContext from "./CustomHooks/VariablesContext";
-import Transactions from "./components/Transactions";
-
+import Loginpage from "./components/login/Loginpage"
+import Signuppage from "./components/login/Signuppage"
+import {Routes , Route} from "react-router-dom"
+import Home from "./components/Home";
 function App() {
   return (
-    <Router>
-      <div>
-        <Header /> 
-        <VariablesContext>
-          <div className="container">
-            <div className="child" >
-              <Container />
-              <div className="inc-exp-container">
-                  <IncomeContainer />
-                  <ExpenseContainer />
-              </div>
-              <h3>Add New Transaction</h3>
-              <Form />
-            </div>
-            <div className="child">
-              <h3>History</h3>
-              <Transactions />
-            </div> 
-          </div>
-        </VariablesContext>
-      </div>
-    </Router>
+       <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
   );
 }
 
